3eeeb86d907a2bb2ea8201042ea07274297c9be6
vicagasi <vicagasi@ut.utm.edu>
2023-07-27 14:00:07 -0700

    - Vex now drop Vex wings

--- src/main/java/rivi/rss/SimpleStaffsMod.java
+++ src/main/java/rivi/rss/SimpleStaffsMod.java
@@ -2,6 +2,13 @@ package rivi.rss;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rivi.rss.entity.EntityMod;
@@ -9,8 +16,12 @@ import rivi.rss.item.ItemGroupsMod;
 import rivi.rss.item.ItemMod;
 
 public class SimpleStaffsMod implements ModInitializer {
+
 	public static final String MOD_ID = "rivis-simple-staffs";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	// LOOT IDs
+	private static final Identifier VEX_ID = EntityType.VEX.getLootTableId();
 
 	@Override
 	public void onInitialize() {
@@ -21,5 +32,14 @@ public class SimpleStaffsMod implements ModInitializer {
 		EntityMod.registerEntities();
 		EntityMod.registerEntityRenderers();
 		ItemGroupsMod.registerItemGroups();
+
+		// Loot
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && VEX_ID.equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.with(ItemEntry.builder(ItemMod.VEX_WING));
+				tableBuilder.pool(poolBuilder);
+			}
+		});
 	}
 }
--- src/main/java/rivi/rss/item/staffs/WindStaffItem.java
+++ src/main/java/rivi/rss/item/staffs/WindStaffItem.java
@@ -1,14 +1,24 @@
 package rivi.rss.item.staffs;
 
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.TargetPredicate;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class WindStaffItem extends AbstractStaffItem{
 
@@ -18,6 +28,7 @@ public class WindStaffItem extends AbstractStaffItem{
 
     protected static SoundEvent STAFF_SOUND = SoundEvents.ENTITY_FIREWORK_ROCKET_LAUNCH; //PLACEHOLDER
     protected static double BOOST_HEIGHT = 1.75;
+    protected static double PUSH_AMOUNT = 1;
     protected static int EFFECT_DURATION = 200;
 
     private static final Settings settings = new Settings().maxCount(1).maxDamage(MAX_CHARGE);
@@ -42,11 +53,37 @@ public class WindStaffItem extends AbstractStaffItem{
         return STAFF_SOUND;
     }
 
+    Box SEARCH_BOX = new Box(-105, -102, -105, 105, 102, 105);
+
+    public void windPush(World world, PlayerEntity player){
+
+        double pX = player.getX();
+        double pY = player.getY();
+        double pZ = player.getZ();
+
+        Entity entity = world.getClosestEntity(LivingEntity.class, TargetPredicate.DEFAULT, null,
+                pX, pY, pZ, SEARCH_BOX);
+
+        if(entity != null){
+
+            double eX = entity.getX();
+            double eY = entity.getY();
+            double eZ = entity.getZ();
+
+            Vec3d pushVec = new Vec3d(pX - eX, pY - eY, pY - eZ);
+
+            entity.addVelocity(pushVec);
+            entity.addVelocity(0, 1000, 0); //FIXME
+        }
+    }
+
     @Override
     public void staffEffect(ItemStack itemStack, World world, PlayerEntity player) {
+        windPush(world, player);
         player.addVelocity(0, BOOST_HEIGHT, 0);
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOW_FALLING, EFFECT_DURATION));
         world.addParticle(ParticleTypes.EXPLOSION, //PLACEHOLDER
                 player.getX(), player.getY() + 1, player.getZ(), 0, BOOST_HEIGHT, 0);
+
     }
 }
